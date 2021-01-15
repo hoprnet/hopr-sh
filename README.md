248873b5f3ef68c9ba6368a82a1740ea2c21d31d
nionis <stivennoni77@gmail.com>
2021-01-15 16:56:03 +0200

    simplify command

--- README.md
+++ README.md
@@ -1,26 +1,34 @@
 # HOPR SH
 
-## install-uni instructions:
+## Setup and install HOPRd
 
-log in to vps
+Type following commands into your terminal, if you are using a VPS, log in into your VPS.
 
-* wget https://github.com/hoprnet/hopr-sh/blob/master/install-uni.sh
-* chmod +x install-uni.sh
-* ./install-uni.sh
+```bash
+$ wget https://github.com/hoprnet/hopr-sh/blob/master/setup-hoprd.sh
+$ chmod +x setup-hoprd.sh
+$ ./setup-hoprd.sh
+```
 
-### to run chat in local browser
+## Running HOPRd
 
-* ssh -L 3000:0.0.0.0:3000 root@`<ip address of VPS>`
-* you'll then be prompted to enter your password
+With this command, we will run hoprd and store logs.
 
-### if users need to save logs primitively
+```bash
+DEBUG=hopr*,libp2p:mplex:stream hoprd --init --data /hoprd --rest --admin 2> titlis[$(hoprd --version)].log.txt
+```
 
-in the *local* computer:
+### Accessing HOPRd on a VPS
 
-* scp `<username>`@`<ip address>`:/hopr-chat/log.txt ~/Desktop
+```bash
+$ ssh -L 3000:0.0.0.0:3000 root@`<VPS ip address>`
+# you'll then be prompted to enter your password
+```
 
-`<username>` is the vps username, probably `root`
+### Save logs from a VPS
 
-`<ip address>` is the vps ip address
+in the _local_ computer:
 
-they will be prompted to enter the vps password and then download will begin
+```bash
+$ scp `<VPS username>`@`<VPS ip address>`:/hoprd/log.txt ~/
+```
--- setup-hoprd.sh
+++ setup-hoprd.sh
@@ -18,28 +18,20 @@ do
     case $response in
         y) echo Terrific!
         echo -e ${RED}Warning! Running this script repeatedly will cause you to have a new node address each time.${NC}
-        while [ "$response" != "n" ]
-        do
-            read -p "Would you like to run this script? [y/n]:" response
-            case $response in
-                y) echo What release are you installing? Format: X.XX.X
-                   read vrelease
-                   [ ! -d "root/db-old$((`ls root | wc -w`))" ] && mv -f root/db root/db-old$((`ls root | wc -w`))
-                   sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
-                   curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-                   sudo apt -y install gcc g++ make
-                   sudo apt -y install nodejs
-                   sudo npm install --unsafe-perm=true -g @hoprnet/hoprd@$vrelease
-                   DEBUG=hopr*,libp2p:mplex:stream hoprd --init --rest --admin 2> titlis[$vrelease].log.txt
-                            ;;
-                n)
-                    echo "Goodbye!"
-                    exit ;;
-            esac
-        done
-                    ;;
-        n)
-            echo "Goodbye!"
-            exit ;;
+
+        read -p "Would you like to run this script? [y/n]:" response
+        case $response in
+            y) echo What release are you installing? Format: X.XX.X \(https://github.com/hoprnet/hoprnet/releases\)
+                read vrelease
+                sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+                curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+                sudo apt -y install gcc g++ make
+                sudo apt -y install nodejs
+                sudo npm install --unsafe-perm=true -g @hoprnet/hoprd@$vrelease
+                exit ;;
+            n)
+                echo "Goodbye!"
+                exit ;;
+        esac
     esac
 done

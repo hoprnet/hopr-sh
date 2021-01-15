513e602630d460df6b8212072017db1a33cb7cd6
nionis <stivennoni77@gmail.com>
2021-01-15 16:06:19 +0200

    remove hopr-chat and old testnet

--- install_run_larnaca.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt-get update && apt-get upgrade -y
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-apt-get install -y nodejs
-npm install --unsafe-perm=true -g @hoprnet/hoprd@1.57.7
-DEBUG=hopr* hoprd --init --admin
--- setup-hoprd.sh
+++ setup-hoprd.sh
@@ -4,9 +4,14 @@ RED='\033[0;91m'
 CYAN='\033[0;96m'
 YELLOW='\033[1;93m'
 NC='\033[0m' # No Color
+
+# print HOPR logo
 echo -e "
 ${YELLOW}██╗  ██╗ ██████╗ ██████╗ ██████╗ \n██║  ██║██╔═══██╗██╔══██╗██╔══██╗\n███████║██║   ██║██████╔╝██████╔╝\n██╔══██║██║   ██║██╔═══╝ ██╔══██╗\n██║  ██║╚██████╔╝██║     ██║  ██║\n╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝  ╚═╝\n${NC}"
+
+# print policy warning
 echo -e ${CYAN}By installing this node, you agree to our Privacy Policy found at https://hoprnet.org/privacy-policy${NC}
+
 while [ "$response" != "n" ]
 do
     read -p "Do you agree to our Privacy Policy? [y/n]:" response
@@ -22,7 +27,7 @@ do
                    [ ! -d "root/db-old$((`ls root | wc -w`))" ] && mv -f root/db root/db-old$((`ls root | wc -w`))
                    sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
                    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-                   sudo apt -y  install gcc g++ make
+                   sudo apt -y install gcc g++ make
                    sudo apt -y install nodejs
                    sudo npm install --unsafe-perm=true -g @hoprnet/hoprd@$vrelease
                    DEBUG=hopr*,libp2p:mplex:stream hoprd --init --rest --admin 2> titlis[$vrelease].log.txt
